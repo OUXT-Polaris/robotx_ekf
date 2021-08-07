09247fdb51cf7a95a285c3d3ef505c76954a9a66
kentaokamoto <kenntabsk@gmail.com>
2021-08-07 22:43:16 +0900

    fix

--- src/ekf_component.cpp
+++ src/ekf_component.cpp
@@ -1,9 +1,8 @@
 // Copyright (c) 2020 OUXT Polaris
 //
-//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you mu(1) not use this file except in compliance with the License.
-// You mu(1) obtain a copy of the License at
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -12,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 
 
 #include <robotx_ekf/ekf_component.hpp>
